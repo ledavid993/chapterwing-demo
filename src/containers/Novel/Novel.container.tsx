958a321284455241bccf65a977dc328b173f3668
le.david993@gmail.com <le.david993@gmail.com>
2020-05-25 23:32:05 -0500

    shadow

--- src/containers/Novel/Novel.container.tsx
+++ src/containers/Novel/Novel.container.tsx
@@ -12,12 +12,14 @@ export default function Novel() {
           <div className={styles.paper} />
           <div className={styles.paper} />
           <div className={styles.paper} />
+          <div className={styles.paper} />
           <div className={styles.paper}>
             <Image
               src="https://cdn.novelupdates.com/images/2019/01/The-Second-Coming-of-Avarice.jpg"
               h="100%"
               w="100%"
-              borderColor="background.300"
+              borderRadius="5px"
+              border="1px solid rgba(122,122,122)"
             />
           </div>
         </div>
