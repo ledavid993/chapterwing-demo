9c5046db08d724fff2b14f473c0359dbcf33f7f8
le.david993@gmail.com <le.david993@gmail.com>
2020-06-21 00:48:57 -0500

    base url changes

--- src/redux/services/basehttp.service.ts
+++ src/redux/services/basehttp.service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 
 export default class BaseHttpService {
-  BASE_URL = 'https://localhost:3000';
+  BASE_URL = 'http://wingpen-env.eba-8uiqhem9.us-east-2.elasticbeanstalk.com';
   _accessToken: string | null = null;
 
   constructor() {}
