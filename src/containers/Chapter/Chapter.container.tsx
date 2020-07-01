8b022d6eff92830b411c165146dda38bb799205d
le.david993@gmail.com <le.david993@gmail.com>
2020-07-01 10:31:30 -0500

    volume title in chapters

--- src/containers/Chapter/Chapter.container.tsx
+++ src/containers/Chapter/Chapter.container.tsx
@@ -4,14 +4,19 @@ import { Layout } from '@components';
 import { NovelState } from '@interface/novel.interface';
 import { nodesToHtml } from '@utils';
 import styles from './Chapter.module.scss';
+import { useRouter } from 'next/router';
 
 const Chapter = () => {
   const novelState: NovelState = useSelector(({ novel }: any) => novel);
   const { currentChapter } = novelState;
+  const router = useRouter();
 
   return (
     <Layout>
       <Box className={styles.contentContainer}>
+        <Heading display="flex" justifyContent="center" alignItems="center" color="#d3d3d3">
+          {router.query.volumeTitle}
+        </Heading>
         <Heading color="white" fontSize="1.5rem" marginTop="25px">
           Chapter {currentChapter?.chapterNumber}: {currentChapter?.title}
         </Heading>
