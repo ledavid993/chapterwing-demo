2dc4cbcbf2c45463909ef12c31ff74232a0bea78
le.david993@gmail.com <le.david993@gmail.com>
2020-07-03 03:10:13 -0500

    chapter navigation

--- src/containers/Chapter/Chapter.container.tsx
+++ src/containers/Chapter/Chapter.container.tsx
@@ -1,15 +1,25 @@
-import { Heading, Box, Text } from '@chakra-ui/core';
+import { Heading, Box, Text, Flex, Button } from '@chakra-ui/core';
 import { useSelector } from 'react-redux';
 import { Layout } from '@components';
 import { NovelState } from '@interface/novel.interface';
 import { nodesToHtml } from '@utils';
 import styles from './Chapter.module.scss';
 import { useRouter } from 'next/router';
+import { navigateToChapterPage } from '../../utils/navigate';
 
 const Chapter = () => {
   const novelState: NovelState = useSelector(({ novel }: any) => novel);
   const { currentChapter } = novelState;
   const router = useRouter();
+
+  const onNavigateChapter = (change: number) => {
+    navigateToChapterPage(
+      router,
+      '/novels',
+      router.query.novel,
+      currentChapter.chapterNumber + change
+    );
+  };
 
   return (
     <Layout>
@@ -29,6 +39,15 @@ const Chapter = () => {
         </Heading>
         <Box marginTop="60px" />
         <div dangerouslySetInnerHTML={{ __html: nodesToHtml(currentChapter?.document) }} />
+        <Flex justifyContent="space-between" marginTop="10px">
+          <Button
+            isDisabled={currentChapter.chapterNumber <= 1}
+            onClick={() => onNavigateChapter(-1)}
+          >
+            Prev
+          </Button>
+          <Button onClick={() => onNavigateChapter(1)}>Next</Button>
+        </Flex>
       </Box>
     </Layout>
   );
