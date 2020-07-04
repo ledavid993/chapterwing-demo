ed833dbea942e6118c564f4e590364cd16e97708
le.david993@gmail.com <le.david993@gmail.com>
2020-07-04 08:46:01 -0500

    updated conf

--- src/redux/services/basehttp.service.ts
+++ src/redux/services/basehttp.service.ts
@@ -3,7 +3,9 @@ import jwtDecode from 'jwt-decode';
 
 export default class BaseHttpService {
   BASE_URL =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.BASE_URL;
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : process.env.NEXT_PUBLIC_BASE_URL;
   _accessToken: string | null = null;
 
   constructor() {}
